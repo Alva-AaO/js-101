cf55ccbaf65081ea40de7e6b7da9da28d0e819aa
Agustin Alvarez Ovando <agusovando@gmail.com>
2021-01-18 23:29:51 -0300

    inicial
    		# with '#' will be ignored, and an empty message aborts the commit.

---
+++ clase1.js
@@ -0,0 +1,21 @@
+
+//float
+//edadCliente = 30
+
+//string
+//nombreCliente = "Agustin"
+//apellidoCliente = "Alvarez"
+
+identificarNombre = prompt("Ingrese su nombre aquí")
+identificarEdad = prompt("Ingrese su edad aquí")
+
+//parse = convertir int = numero
+edad = parseInt(identificarEdad) + 10
+
+//tmb se puede: identificarEdad = parseInt(prompt("Ingrese su edad aqui")); para hacerlo en una linea.
+
+//Console: Una manera automatica de ver el valor de una variable
+console.log("La edad del cliente es: " + edad + " años")
+
+//Alert
+alert("Fin de programa")
